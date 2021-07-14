4a04b96f15926acd97c080d45d29f8dc625a854b
Ronnie Rios <ronniesmail14@gmail.com>
2021-07-14 16:00:50 -0500

    find by userID works, added route and method in controller

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,11 +1,16 @@
 const router = require('express').Router();
 const {
     getAllUsers,
+    getUserById,
     createUser
 } = require('../../controllers/user-controller');
 
+// /users
 router.route('/')
     .get(getAllUsers)
     .post(createUser);
-
+    
+// /users/:id endpoint
+router.route('/:id')
+    .get(getUserById);
 module.exports = router;
