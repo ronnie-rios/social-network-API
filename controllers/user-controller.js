f660b35a3691e965138673665098f8b5f2feb17a
Ronnie Rios <ronniesmail14@gmail.com>
2021-07-14 16:07:14 -0500

    added update and delete methods to user-controller function

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -14,7 +14,7 @@ const userController = {
     getUserById({ params }, res) {
         User.findOne({ _id: params.id })
         .then(dbUserData => {
-            //if no pizza 404
+            //if no user 404
             if(!dbUserData) {
                 res.status(404).json({ message: 'no user with this id'});
                 return
@@ -30,6 +30,40 @@ const userController = {
         User.create(body)
         .then(dbUserData => res.json(dbUserData))
         .catch(err => res.status(400).json(err))
+    },
+    //update user by id
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate(
+            {_id: params.id },
+            body, //add validators?
+            {new: true}
+        ).then(dbUserData => {
+            //if no user 404
+            if(!dbUserData) {
+                res.status(404).json({ message: 'no user with this id'});
+                return
+            }
+            res.json(dbUserData)
+        }).catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+          });
+    },
+
+    //delete a user
+    deleteUser({ params }, res ) {
+        User.findOneAndDelete({ _id: params.id })
+        then(dbUserData => {
+            //if no user 404
+            if(!dbUserData) {
+                res.status(404).json({ message: 'no user with this id'});
+                return
+            }
+            res.json(dbUserData)
+        }).catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+          });
     }
 };
 
