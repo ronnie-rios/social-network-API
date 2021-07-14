9c7944629d7a3cced9c58339bcbba3b617d30d32
Ronnie Rios <ronniesmail14@gmail.com>
2021-07-14 17:06:10 -0500

    addex thought model export to index on /models, created first thought . find function

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,7 +1,15 @@
-const { Thought } = require('../models');
+const { Thought, User } = require('../models');
 
 const thoughtController = {
-
+    //get all thoughts
+    getAllThoughts(req, res) {
+        Thought.find({})
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err=> {
+            console.log(err);
+            res.status(400).json(err);
+        })
+    }
 }
 
 module.exports = thoughtController;
